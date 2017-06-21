f301a38df70647d8d496f496b806ca87d5400bd0
Daniel Paredes <hparedes32>
2017-06-21 08:08:16 +0200

    add servlet mailservice

--- src/main/java/com/main/Main.java
+++ src/main/java/com/main/Main.java
@@ -1,7 +1,20 @@
 package com.main;
 
 import com.entity.Course;
+import com.repository.CourseDAO;
+import com.repository.HibernateUtil;
 import com.service.CRUDService;
+import com.service.MailService;
+import org.hibernate.Hibernate;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+
+import javax.management.Query;
+import javax.persistence.EntityManager;
+import javax.security.auth.login.AppConfigurationEntry;
+import javax.security.auth.login.Configuration;
+import javax.swing.*;
 
 /**
  * Created by Daniel Paredes on 7/4/2017.
@@ -9,11 +22,8 @@ import com.service.CRUDService;
 public class Main {
     public static void main(String[] args) {
 
-        CRUDService nuevocurso = new CRUDService();
-
-        nuevocurso.addCourse("LO02","Anglais");
 
 
 
+        }
     }
-}
--- src/main/java/com/servlet/AddClient.java
+++ src/main/java/com/servlet/AddClient.java
@@ -3,14 +3,19 @@ package com.servlet;
 import com.entity.Client;
 import com.entity.Course;
 import com.repository.ClientDAO;
+import com.repository.HibernateUtil;
 import com.service.CRUDService;
+import com.service.MailService;
 import com.service.SubscribeService;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
 
 import javax.naming.ldap.LdapName;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.swing.*;
 import java.io.IOException;
 import java.io.PrintWriter;
 
@@ -21,7 +26,7 @@ public class AddClient extends HttpServlet {
     public void doGet(HttpServletRequest request, HttpServletResponse response ) throws ServletException, IOException {
         response.setContentType("text/html");
 
-        response.setCharacterEncoding( "UTF-8" );
+        response.setCharacterEncoding("UTF-8");
 
         PrintWriter out = response.getWriter();
 
@@ -46,17 +51,31 @@ public class AddClient extends HttpServlet {
         out.println("</html>");
 
 
-        CRUDService nuevocurso = new CRUDService();
+        try {
 
-        nuevocurso.addCourse("LO56","Anglais");
-
-        response.sendRedirect(request.getContextPath()+"/index.jsp");
-
+            Session session = HibernateUtil.getSessionFactory().openSession();
+            session.beginTransaction();
 
 
-
+            Client cliente1= new Client(null,"Perez","Juan","4 rue Belgrano","danielcabj32@gmail.com","0769261065","3");
 
 
 
+            MailService nuevomail = new MailService();
+            nuevomail.sendingSubscriptionMail(cliente1);
+
+
+            session.save(nuevomail);
+
+            session.getTransaction().commit();
+
+            session.close();
+
+        } catch (HibernateException e) {
+            JOptionPane.showMessageDialog(null, "Error occured !");
+            e.printStackTrace();
+
+
+        }
     }
 }
